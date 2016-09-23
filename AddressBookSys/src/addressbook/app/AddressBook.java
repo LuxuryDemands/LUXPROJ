c368c6329314af941255cf39c920d898858853b5
1541986 <1541986@2F16-30.ad.dawsoncollege.qc.ca>
2016-09-23 18:38:51 -0400

    test

--- AddressBookSys/src/addressbook/app/AddressBook.java
+++ AddressBookSys/src/addressbook/app/AddressBook.java
@@ -7,6 +7,7 @@ public class AddressBook {
 	public static void main(String[] args) {
 		Address myAddress = new Address("3040", "Sherbrooke", "Westmount");
 		System.out.println(myAddress.getAddress());
+		System.out.println("ITS FUCKING WORKING");
 
 	}
 
